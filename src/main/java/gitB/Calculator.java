3cf969c50a476774e9258f032eeb112b1719cc63
pms <cjswotls2@naver.com>
2016-04-27 15:24:36 +0900

    뺼셈기능추가

--- src/main/java/gitB/Calculator.java
+++ src/main/java/gitB/Calculator.java
@@ -1,13 +1,17 @@
 package gitB;
 
 public class Calculator {
-	private int result=0;
-	
+	private int result = 0;
+
 	public void add(int x, int y) {
-		result=x+y;
+		result = x + y;
 	}
 
-	public int getResult()  {
+	public void sub(int x, int y) {
+		result = x - y;
+	}
+
+	public int getResult() {
 		return result;
 	}
 }
