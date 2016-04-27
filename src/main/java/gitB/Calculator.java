c2b0a6c973daea78f6795db284346a0632192f9a
pms <cjswotls2@naver.com>
2016-04-27 16:20:48 +0900

    .travis.yml 추가

--- src/main/java/gitB/Calculator.java
+++ src/main/java/gitB/Calculator.java
@@ -1,13 +1,13 @@
 package gitB;
 
 public class Calculator {
-	private int result=0;
-	
+	private int result = 0;
+
 	public void add(int x, int y) {
-		result=x+y;
+		result = x + y;
 	}
 
-	public int getResult()  {
+	public int getResult() {
 		return result;
 	}
 }
